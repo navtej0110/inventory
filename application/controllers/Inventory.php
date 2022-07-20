bffee1f3ed5db3cfd07eaf833dd0acb46e51c430
Navtej Singh <nvtej.0110@gmail.com>
2022-07-20 17:42:18 +0530

    Task 3.2 Active/Verified Attached Users

--- application/controllers/Inventory.php
+++ application/controllers/Inventory.php
@@ -10,8 +10,14 @@ class Inventory extends CI_Controller {
 
 	public function dashboard()
 	{      
+        // 3.1 Count of all active and verified users.
         $thisQuery = $this->db->query("SELECT count(*) as active_verified_users from `users` WHERE `status` = 1 AND `verified` = 1")->result(); 
         $data['active_verified_users'] = $thisQuery[0]->active_verified_users;
+
+        // 3.2  Count of active and verified users who have attached active products.
+        $active_verified_attached_users = $this->db->query("SELECT u.user_id FROM `users` u INNER JOIN `user_product_list`up ON (up.user_id = u.user_id) INNER JOIN `products` p ON (p.id = up.product_id) WHERE  u.`status` = 1 AND u.`verified` = 1     AND p.`status` = 'active' GROUP BY u.user_id")->num_rows(); 
+        $data['active_verified_attached_users'] = $active_verified_attached_users;
+
 		$this->load->view('inventory/dashboard', $data);
 	}
 }
